4e07843820efa1ba6516507e6c4606d9e921ed24
alirazagpt4 <Escape>
2025-07-18 16:53:19 +0500

    chnages in nav bar with toggle unc

--- src/app/Components/header/header.ts
+++ src/app/Components/header/header.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatNavList } from '@angular/material/list';
+import { MatSidenav, MatSidenavContainer } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+MatNavList
+
 
 
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule , RouterModule],
+  imports: [MatToolbarModule , RouterModule , MatNavList , MatSidenav , MatSidenavContainer,MatIcon],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
@@ -16,5 +21,11 @@ export class Header {
     console.log('Button clicked ✅');
   }
   
+
+  mobileMenuOpen = false;
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
 
 }
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { Header } from './Components/header/header';
 import { Footer } from './Components/footer/footer';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , Header , Footer],
+  imports: [RouterOutlet , Header , Footer , MatSidenavModule , MatToolbarModule , MatIconModule , MatListModule,MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
