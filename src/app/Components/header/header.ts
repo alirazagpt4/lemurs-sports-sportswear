c1ff7f821827a94ce49aaa6c23d0c8932e4558be
alirazagpt4 <Escape>
2025-07-19 13:07:16 +0500

    chnages in nav bar with toggle unc

--- src/app/Components/header/header.ts
+++ src/app/Components/header/header.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatNavList } from '@angular/material/list';
 import { MatSidenav, MatSidenavContainer } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
-MatNavList
+// CommonModule
+// MatNavList
 
 
 
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule , RouterModule , MatNavList , MatSidenav , MatSidenavContainer,MatIcon],
+  imports: [MatToolbarModule , RouterModule , MatNavList , MatSidenav , MatSidenavContainer,MatIcon , CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
-export class Header {
+export class Header implements OnInit {
 
   testClick() {
     console.log('Button clicked ✅');
@@ -27,5 +29,17 @@ export class Header {
   toggleMobileMenu() {
     this.mobileMenuOpen = !this.mobileMenuOpen;
   }
+
+
+  isMobileView = false;
+
+ngOnInit() {
+  this.checkMobileView();
+  window.addEventListener('resize', () => this.checkMobileView());
+}
+
+checkMobileView() {
+  this.isMobileView = window.innerWidth <= 768;
+}
 
 }
